c7e3f01a1bbd60e75bcb6c4e712ffee3b6371464
annacarolinaoc <annacarolina_oc@icloud.com>
2025-10-01 20:15:56 -0300

    Add files via upload

---
+++ ConsumerViaCep/ConsumerViaCep/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using ConsumerViaCep.Models;
+using static System.Console;
+
+WriteLine("Digite o seu cep: ");
+var cep = ReadLine();
+
+var enderecoUrl = $@"https://viacep.com.br/ws/{cep}/json/";
+
+var client = new HttpClient();
+
+try
+{
+    HttpResponseMessage? respostaApi = await client.GetAsync(enderecoUrl);
+
+    respostaApi.EnsureSuccessStatusCode();
+
+    string respostaApiJson = await respostaApi.Content.ReadAsStringAsync();
+
+    Endereco? endereco = JsonSerializer.Deserialize<Endereco>(respostaApiJson);
+
+    WriteLine("CEP:" + endereco.cep);
+    WriteLine("Logradouro:" + endereco.logradouro);
+    WriteLine("Bairro:" + endereco.bairro);
+    WriteLine("Cidade:" + endereco.localidade);
+}
+catch (System.Exception e)
+{
+
+    WriteLine("Aconteceu um erro:\n" + e.Message);
+}
